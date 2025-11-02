c17b23ed799f1b066a5b4ded0e6111e1a88ab5e6
Windmillcode <restop-4512@restop4512sair.mynetworksettings.com>
2025-11-02 06:36:36 -0500

    [UPDATE] additional work

--- src/utils/test.ts
+++ src/utils/test.ts
@@ -161,7 +161,7 @@ export const createFileCellConfig = (item: Partial<FileConfig>): FileConfig => (
 /**
  * Create Column Config
  */
-            // @ts-ignore
+// @ts-ignore
 export const createColumnConfig = (item: Partial<ColumnConfig> = {}): ColumnConfig => ({
   enabled: true,
   field: {
